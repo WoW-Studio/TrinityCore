1a51797b5e46d18d55df2e4152196e19cee34a20
Cashew <19Roca91@web.de>
2014-09-18 12:38:54 +0200

    Typo

--- sql/wow-studio/world/2014_09_04_00_world_misc.sql
+++ sql/wow-studio/world/2014_09_04_00_world_misc.sql
@@ -175,7 +175,7 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 
 DELETE FROM creature_equip_template WHERE entry=2057;
 
-UPDATE `creature_loot_template` SET `ChanceOrQuestChance`=6 WHERE `entry` IN (34566, 35216, 35268, 35269, 35347, 35348, 35349, 35350, 35351, 35352, 35447, 35448, 35449, 35615, 35616) AND `item`=3
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`=6 WHERE `entry` IN (34566, 35216, 35268, 35269, 35347, 35348, 35349, 35350, 35351, 35352, 35447, 35448, 35449, 35615, 35616) AND `item`=3;
 
 -- Marin Noggenfogger SAI
 SET @ENTRY := 7564;
